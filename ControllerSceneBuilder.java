8fb05b30ec432929b2802e57297b9008db9a7184
mc3reese <130010097+mc3reese@users.noreply.github.com>
2023-04-26 21:50:52 -0400

    Added FXML file and SceneBuilder GUI

---
+++ ControllerSceneBuilder.java
@@ -0,0 +1,38 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+public class ControllerSceneBuilder {
+
+    @FXML
+    private ImageView dogImage;
+
+    @FXML
+    private Button falseButton;
+
+    @FXML
+    private Label questionBanner;
+
+    @FXML
+    private ProgressBar questionProgress;
+
+    @FXML
+    private Label questionText;
+
+    @FXML
+    private Button trueButton;
+
+    @FXML
+    void checkFalse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkTrue(ActionEvent event) {
+
+    }
+
+}
--- DogGameGUI.java
+++ DogGameGUI.java
@@ -22,8 +22,8 @@ import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
-
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 
 public class DogGameGUI extends Application
@@ -39,48 +39,53 @@ public class DogGameGUI extends Application
    
    
    @Override
-   public void start(Stage primaryStage)
+   public void start(Stage primaryStage) throws Exception
    {
-   
+      
+      Parent root = FXMLLoader.load(getClass().getResource("FinalSceneBuilder.fxml"));    
+      Scene scene = new Scene(root);   
+      
+      
       //stage title
       primaryStage.setTitle("Dog Gone: Dog Facts!");
-      
-      //Create a Label control, message, aligns top of page
-      Label bannerText = new Label("Question:"); 
+//     Create a Label control, message, aligns top of page
+//       Label bannerText = new Label("Question:"); 
+// 
+// 
+// 
+// 
+//       Middle of Page True Button prompt
+//       Button trueButton = new Button("ITS A FACT!");
+//       Middle of Page False Button Prompt
+//       Button falseButton = new Button("ITS FALSE!");
+//       HBox hbox = new HBox(30, trueButton, falseButton);
+//       hbox.setAlignment(Pos.TOP_CENTER);
+//       
+//       
+//       
+//       *******Image Code goes here********
+//       /*
+//       Image dogImage = new Image("dogPhotos");
+//       ImageView dogImage = new ImageView(dogImage);
+//       
+//       image.setPreserveRatio(true);
+//       */
+//       
+//       
+//       VBox
+//       VBox vbox = new VBox(100, bannerText, hbox);
+//       vbox.setAlignment(Pos.CENTER);
+//       
+//       
+//       
+//       
+//       /* Creates a scene with the Hbox as its root node. 
+//       800x600 resolution */
+//       Scene scene = new Scene(vbox, 600, 400);
+//       
+//
 
-
-
-
-      //Middle of Page True Button prompt
-      Button trueButton = new Button("ITS A FACT!");
-      //Middle of Page False Button Prompt
-      Button falseButton = new Button("ITS FALSE!");
-      HBox hbox = new HBox(30, trueButton, falseButton);
-      hbox.setAlignment(Pos.TOP_CENTER);
-      
-      
-      
-      //*******Image Code goes here********
-      /*
-      Image dogImage = new Image("dogPhotos");
-      ImageView dogImage = new ImageView(dogImage);
-      
-      image.setPreserveRatio(true);
-      */
-      
-      
-      //VBox
-      VBox vbox = new VBox(100, bannerText, hbox);
-      vbox.setAlignment(Pos.CENTER);
-      
-      
-      
-      
-      /* Creates a scene with the Hbox as its root node. 
-      800x600 resolution */
-      Scene scene = new Scene(vbox, 600, 400);
-      
-      
+       
       //Adds the scene to the stage
       primaryStage.setScene(scene);
       
@@ -89,16 +94,5 @@ public class DogGameGUI extends Application
             
    }
    
-   /*Event handler for Button
-   class ButtonClickHandler implements EventHandler
-   {
-   
-      @Override
-      public void handle(ActionEvent event)
-      {
-         myLabel.setText("That's Correct!");
-      }
-      
-   }
-   */
+
 }
