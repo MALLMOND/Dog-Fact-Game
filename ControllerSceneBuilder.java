4a8c75c76591e131b5bd902e21026d6f478e2078
Michael <michaelallmond1@gmail.com>
2023-04-26 22:03:06 -0400

    Update ControllerSceneBuilder.java

--- ControllerSceneBuilder.java
+++ ControllerSceneBuilder.java
@@ -24,6 +24,12 @@ public class ControllerSceneBuilder {
 
     @FXML
     private Button trueButton;
+    
+    private String[] dogPhotos = {"https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190602_204319.jpg",
+                                 "https://images.dog.ceo/breeds/retriever-curly/n02099429_3026.jpg",
+                                 "https://images.dog.ceo/breeds/sharpei/noel.jpg",
+                                 "https://images.dog.ceo/breeds/terrier-tibetan/n02097474_4875.jpg",
+                                 "https://images.dog.ceo/breeds/doberman/n02107142_3488.jpg"};
 
     @FXML
     void checkFalse(ActionEvent event) {
